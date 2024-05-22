3a13bc064aa719e3368512128a939b1f16067ece
Shariq Shahzad <shariq.shahzad@royalcyber.com>
2024-05-22 11:30:10 +0500

    layout changes

---
+++ src/app/shared/components/AdminLayout/Navigation.ts
@@ -0,0 +1,102 @@
+export const navigations = [{
+    title:'Dashboard',
+    link:''
+},
+{
+    title:'Policies',
+    subMenu:[
+        {
+            title:'Standard Policies',
+            link:''
+        },
+        {
+            title:'Policy Review Requests   ',
+            link:''
+        },
+        {
+            title:'Old Policies',
+            link:''
+        }
+    ]
+},
+{
+    title:'Companies',
+    subMenu:[
+        {
+            title:'Companies',
+            link:''
+        },
+        {
+            title:'Company Clerks',
+            link:''
+        }
+    ]
+},
+{
+    title:'Law Offices',
+    subMenu:[
+        {
+            title:'Lawyers',
+            link:''
+        },
+        {
+            title:'Law Office Infos',
+            link:''
+        },
+        {
+            title:'Check Eligibility Responses',
+            link:''
+        }
+    ]
+},
+{
+    title:'Cities',
+    link:''
+},
+{
+    title:'Admin Users',
+    link:''
+},
+{
+    title:'Company Status',
+    link:''
+},
+{
+    title:'Payments',
+    subMenu:[
+        {
+            title:'Invoices',
+            link:''
+        },
+        {
+            title:'Payment Logs',
+            link:''
+        },
+        {
+            title:'Payment Status Logs',
+            link:''
+        },
+        {
+            title:'Unified Invoices Logs',
+            link:''
+        }
+    ]
+},
+{
+    title:'MHRSD',
+    link:''
+},
+
+{
+    title:'Settings',
+    link:''
+},
+{
+    title:'Activity Records',
+    link:''
+},
+{
+    title:'Statistics',
+    link:''
+},
+]
